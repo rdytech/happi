9f86341db1cd3d7042562b90ad862d3cc3f310af
Sam Giffney <samg@jobready.com.au>
2018-02-23 16:08:29 +1100

    include development gems in build

--- .buildkite/scripts/tests.sh
+++ .buildkite/scripts/tests.sh
@@ -7,7 +7,7 @@ rbenv local 2.1.5
 rbenv rehash
 
 echo '--- bundling'
-bundle install -j $(nproc) --without development production --quiet
+bundle install -j $(nproc) --without production --quiet
 
 echo '--- running specs'
 bundle exec rspec
