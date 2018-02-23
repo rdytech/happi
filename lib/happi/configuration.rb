dd2e01eecc6b493420d47fc88d2af72dbf761a62
Michael MacDonald <michael@starclass.com.au>
2018-02-23 17:18:36 +1100

    Use token type (#14)
    * Pass token_type option into FaradayMiddleware::OAuth2
    * Bump to version 0.3.0
    * Add Changelog

--- lib/happi/configuration.rb
+++ lib/happi/configuration.rb
@@ -1,16 +1,23 @@
 class Happi::Configuration
   def self.defaults
-     {
-    host: 'http://localhost:8080',
-    port: 443,
-    timeout: 60,
-    version: 'v1',
-    use_json: false,
-    log_level: :info
+    {
+      host: 'http://localhost:8080',
+      port: 443,
+      timeout: 60,
+      version: 'v1',
+      use_json: false,
+      log_level: :info
     }
   end
 
-  attr_accessor :oauth_token, :host, :port, :timeout, :version, :use_json, :log_level
+  attr_accessor :oauth_token,
+                :host,
+                :port,
+                :timeout,
+                :version,
+                :use_json,
+                :log_level,
+                :token_type
 
   def initialize(options = {})
     self.class.defaults.merge(options).each do |key, value|
